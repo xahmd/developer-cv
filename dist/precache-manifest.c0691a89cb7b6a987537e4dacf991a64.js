008bdb9eafc9e3fdc2effab2d613bcced0de5b5a
ahmedmohja <hfl@outlook.sa>
2021-06-13 00:13:49 +0300

    fix some builds

---
+++ dist/precache-manifest.c0691a89cb7b6a987537e4dacf991a64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0bf76b9c0ca57cb04a",
+    "url": "/main/css/app.4a9c3239.css"
+  },
+  {
+    "revision": "ebdde4a971469fefd95d8eede68ca93a",
+    "url": "/main/img/bg.ebdde4a9.jpg"
+  },
+  {
+    "revision": "9fdc0822aee19e2ef3c939ba709e1ba6",
+    "url": "/main/index.html"
+  },
+  {
+    "revision": "fb0bf76b9c0ca57cb04a",
+    "url": "/main/js/app.473fc6c0.js"
+  },
+  {
+    "revision": "05c3878e7e706b800351",
+    "url": "/main/js/chunk-vendors.0785baf1.js"
+  },
+  {
+    "revision": "f08db9ba9ebfc560373a9a4eac90f68a",
+    "url": "/main/manifest.json"
+  }
+]);
